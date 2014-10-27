65a010c14402afa579df3804bd850708e5c58336
Andreas Mauczka <andreas.mauczka@inso.tuwien.ac.at>
2014-10-27 13:48:27 +0100

    Merged filemanagement for setup of maven

--- src/at/ac/tuwien/inso/subcat/miner/SvnMiner.java
+++ src/at/ac/tuwien/inso/subcat/miner/SvnMiner.java
@@ -36,15 +36,34 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 import at.ac.tuwien.inso.subcat.utility.XmlReader;
 import at.ac.tuwien.inso.subcat.utility.XmlReaderException;
+import at.ac.tuwien.inso.subcat.utility.Lemmatizer;
+import at.ac.tuwien.inso.subcat.model.Commit;
 import at.ac.tuwien.inso.subcat.model.Identity;
+import at.ac.tuwien.inso.subcat.model.ManagedFile;
 import at.ac.tuwien.inso.subcat.model.Model;
 import at.ac.tuwien.inso.subcat.model.Project;
 import at.ac.tuwien.inso.subcat.model.User;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.EmptyTreeIterator;
+
 
 public class SvnMiner extends Miner {
 
@@ -52,6 +71,8 @@ public class SvnMiner extends Miner {
 	private Project project;
 	private Model model;
 	private XmlReader reader;
+	private Lemmatizer lemmatizer;
+	private boolean stopped;
 	
 	public SvnMiner (Settings settings, Project project, Model model) {
 		assert (settings != null);
@@ -61,6 +82,9 @@ public class SvnMiner extends Miner {
 		this.settings = settings;
 		this.project = project;
 		this.model = model;
+		
+		//initialize lemmatizer once - this seems to be memory intensive
+		lemmatizer = new Lemmatizer();
 	}
 	
 	//
@@ -89,34 +113,97 @@ public class SvnMiner extends Miner {
 		return identity;
 	}
 	
-	private void processCommit (String aAuthor, Date aDate, String aPath ,String aMsg) throws SQLException, IOException {
+	private class FileStats {
+		public String oldPath;
+		public ChangeType type;
+		
+		public FileStats(String oldPath, ChangeType type){
+			this.oldPath = oldPath;
+			this.type = type;
+		}
+	}
+	
+	private enum ChangeType {ADD, MODIFY, DELETE, RENAME, REPLACE}
+	
+	private void processCommit (String aAuthor, Date aDate, Map <String, FileStats> pathslist ,String aMsg,Map<String, ManagedFile> fileCache) throws SQLException, IOException {
 		Identity author = resolveIdentity (aAuthor);
 		Identity committer = resolveIdentity (aAuthor);
 		
 		Date date = aDate;
 		String msg = aMsg;
-		String path = aPath;
 		
-		assert (date != null || msg != null || path != null);
+		// Lemmatize input - delivers String List, convert it to string
+		msg = StringUtils.join(lemmatizer.lemmatize(msg), ", ");
 		
-		System.out.println ("Commit: " + author);
-		model.addCommit (project, author, committer, date, msg, 0, 0, 0, null);
-		// TODO store path
+		int filecount = pathslist.size();
+		
+		assert (date != null || msg != null || pathslist != null);
+		
+		System.out.println ("Commit: " + msg + " Filecount: " + filecount);
+		Commit commit = model.addCommit (project, author, committer, date, msg, filecount, 0, 0, null);
+		
+		for (Map.Entry<String, FileStats> item : pathslist.entrySet ()) {
+			if (stopped == true) {
+				break;
+			}
+			
+			FileStats stats = item.getValue ();
+			String path = item.getKey ();
+			System.out.println (path);
+			// TODO: binary files
+			
+			switch (stats.type) {
+			case ADD:
+				ManagedFile addedFile = model.addManagedFile (project, path);
+				model.addFileChange (commit, addedFile, 0, 0, 0, 0, 0);
+				fileCache.put (path, addedFile);
+				break;
+
+			case DELETE:
+				ManagedFile deletedFile = fileCache.get (path);
+				assert (deletedFile != null);
+				model.addFileDeletion (deletedFile, commit);
+				fileCache.remove (stats.oldPath);				
+				break;
+
+			case MODIFY:
+				ManagedFile modifiedFile = fileCache.get (path);
+				assert (modifiedFile != null);
+				model.addFileChange (commit, modifiedFile, 0, 0, 0, 0, 0);
+				break;
+
+			case RENAME:
+				ManagedFile renamedFile = fileCache.get (stats.oldPath);
+				assert (renamedFile != null);
+				model.addFileRename (renamedFile, commit, stats.oldPath);
+				fileCache.put (path, renamedFile);
+				fileCache.remove (stats.oldPath);
+				break;
+	
+			default:
+				assert (false);
+			}
+		}
+
+		emitTasksProcessed (1);
 	
 	}
+	
+	
 	
 	public void parse () throws XmlReaderException, ParseException, IOException, SQLException {
 		reader = new XmlReader (settings.srcLocalPath);
 		reader.expectStart ("log", true);
 		reader.acceptStart ("log", true);
+		Map<String, ManagedFile> fileCache = new HashMap<String, ManagedFile> ();
 		while (reader.isStart ("logentry")){
-			parseLogentry ();
+			parseLogentry (fileCache);
 		}
 		reader.expectEnd ("log", true);
 	}
 
 	
-	private void parseLogentry () throws XmlReaderException, ParseException, IOException, SQLException {
+	private void parseLogentry (Map<String, ManagedFile> fileCache) throws XmlReaderException, ParseException, IOException, SQLException {
 		
 		//String revision = reader.getAttribute ("revision");
 		
@@ -124,19 +211,23 @@ public class SvnMiner extends Miner {
 		
 		//<author> might be omitted, e.g. during cvs2svn migration
 		String author = parseOptional ("author");
-		
+		Map <String, FileStats> pathslist = new HashMap <String, FileStats>();
 		//parse date element
 		SimpleDateFormat format = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'");
 		Date date = format.parse (parseSingle("date"));
 		
+		
+		
 		//<paths> must have at least one element <path> but may have more
-		String path = parseMulti (new String[]{"paths","path"});
+		boolean hasFile = parseMulti (new String[]{"paths","path"}, pathslist);
 		
 		//parse commit msg
 		String msg = parseSingle ("msg");
 		
 		//store the commit in the DB
-		processCommit (author, date, path, msg);
+		if(hasFile)
+			processCommit (author, date, pathslist, msg, fileCache);
+		
 		reader.expectEnd ("logentry", true);
 	}
 
@@ -145,6 +236,66 @@ public class SvnMiner extends Miner {
 		String content = reader.getText();
 		reader.expectEnd (element, true);
 		return content;
+	}
+	
+	
+	private class PathEntry {
+		public String newPath;
+		public ChangeType type;
+		public String oldPath;
+		public String copyFromRev;
+		public boolean isFile;
+	}
+	
+
+	private PathEntry parsePath (String element) throws XmlReaderException {
+		
+		PathEntry pathentry = new PathEntry();
+		
+		//Store all attributes before we get Text as it moves on to next tag
+		boolean hasCopyPath = reader.hasAttribute("copyfrom-path");
+		String action = reader.getAttribute("action");
+		pathentry.isFile = reader.getAttribute("kind").equals("file");
+		
+		// TODO Check if we really only care about this, if it is a file		
+		if (hasCopyPath && pathentry.isFile){
+			pathentry.oldPath = reader.getAttribute("copyfrom-path");
+			// we dont care about the revision the file is from not yet
+			//pathentry.copyFromRev = reader.getAttribute("copyfrom-rev");
+		}
+		
+		//step into the text element
+		reader.acceptStart(element, true);
+		pathentry.newPath = reader.getText();
+		reader.acceptEnd (element, true);
+		
+							
+		// If not a rename, the copy from path is always the existing path
+		pathentry.oldPath = pathentry.newPath;
+				
+		switch (action) {
+		case "A":
+			if (!pathentry.oldPath.equals(pathentry.newPath)){
+				pathentry.type = ChangeType.RENAME;
+			}
+			else{
+				pathentry.type = ChangeType.ADD;
+			}
+				
+			break;
+		case "M":
+			pathentry.type = ChangeType.MODIFY;
+			break;
+		case "D":
+			pathentry.type = ChangeType.DELETE;;
+			break;
+		case "R":
+			pathentry.type = ChangeType.REPLACE;
+			break;
+		}
+		
+		
+		return pathentry;
 	}
 	
 	private String parseOptional (String element) throws XmlReaderException {
@@ -157,18 +308,25 @@ public class SvnMiner extends Miner {
 		return content;
 	}
 	
-	private String parseMulti (String [] elements) throws XmlReaderException{
+	private boolean parseMulti (String [] elements, Map <String, FileStats> pathslist) throws XmlReaderException{
 		reader.acceptStart(elements[0],true);
-		String content = parseSingle(elements[1]);
-		try {
-			reader.expectEnd (elements[0], true);
-		}
-		catch (XmlReaderException e){
-			content = parseMulti (elements);
+		
+		//if logentry only has directories, skip logentry
+		boolean hasFile = false;
+		
+		while (reader.isStart(elements[1])){
+			PathEntry pathentry = parsePath(elements[1]);
+			if (!hasFile)
+				hasFile = pathentry.isFile;
+			// TODO Use more of SVN Attributes?
+			if(pathentry.isFile)
+				pathslist.put(pathentry.newPath, new FileStats(pathentry.oldPath, pathentry.type));
 		}
 		
-		return content;
+		reader.acceptEnd(elements[0],true);
+		return hasFile; 
 	}
+	
 	
 	@Override
 	public void run () throws MinerException {
@@ -193,6 +351,7 @@ public class SvnMiner extends Miner {
 		
 	}
 	private void _run () throws IOException, MinerException, SQLException, ParseException, XmlReaderException {
+		
 		parse ();	
 	}
 

66a32f070505877ac6b85a5af1382ed15d1085d1
Florian Brosch <flo.brosch@gmail.com>
2014-11-25 17:35:28 +0100

    reporter: CLI

--- src/at/ac/tuwien/inso/subcat/reporter/Reporter.java
+++ src/at/ac/tuwien/inso/subcat/reporter/Reporter.java
@@ -33,26 +33,26 @@ package at.ac.tuwien.inso.subcat.reporter;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 import at.ac.tuwien.inso.subcat.config.Configuration;
 import at.ac.tuwien.inso.subcat.config.ExporterConfig;
 import at.ac.tuwien.inso.subcat.config.Parser;
 import at.ac.tuwien.inso.subcat.config.ParserException;
 import at.ac.tuwien.inso.subcat.miner.Settings;
-import at.ac.tuwien.inso.subcat.model.Component;
-import at.ac.tuwien.inso.subcat.model.Identity;
 import at.ac.tuwien.inso.subcat.model.Model;
 import at.ac.tuwien.inso.subcat.model.ModelPool;
-import at.ac.tuwien.inso.subcat.model.Priority;
 import at.ac.tuwien.inso.subcat.model.Project;
-import at.ac.tuwien.inso.subcat.model.Severity;
-import at.ac.tuwien.inso.subcat.model.Status;
-import at.ac.tuwien.inso.subcat.model.User;
 
 
 public class Reporter {
@@ -75,14 +75,19 @@ public class Reporter {
 		return formatters;
 	}
 	
-	public void export (ExporterConfig config, Project project, Settings settings, ReportWriter formatter, String outputPath) throws ReporterException {
+	public void export (ExporterConfig config, Project project, int commitDictId, int bugDictId, Settings settings, ReportWriter formatter, String outputPath) throws ReporterException {
 		assert (project != null);
+		assert (project.getId () != null);
 		assert (config != null);
 		assert (settings != null);
 		assert (formatter != null);
+		assert (model != null);
+		assert (config.getQuery () != null);
 		
 		Map<String, Object> vars = new HashMap<String, Object> ();
 		vars.put ("project", project.getId ());
+		vars.put ("commitDict", commitDictId);
+		vars.put ("bugDict", bugDictId);
 
 		try {
 			formatter.init (project, settings, outputPath);
@@ -97,115 +102,236 @@ public class Reporter {
 	}
 
 	public static void main (String[] args) {
-		// Dummy Model:
+		Options options = new Options ();
+		options.addOption ("h", "help", false, "Show this options");
+		options.addOption ("d", "db", true, "The database to process (required)");
+		options.addOption ("p", "project", true, "The project ID to process");
+		options.addOption ("l", "list-projects", false, "List all registered projects");
+		options.addOption ("C", "config", true, "A configuration file including reports");
+		options.addOption ("F", "list-formats", false, "List all supported output formats");
+		options.addOption ("f", "format", true, "Output format");
+		options.addOption ("R", "list-reports", false, "List all report types");
+		options.addOption ("r", "report", true, "Report type");
+		options.addOption ("o", "output", true, "Output path");
+		options.addOption ("c", "commit-dictionary", true, "The commit dictionary ID to use");
+		options.addOption ("b", "bug-dictionary", true, "The bug dictionary ID to use");
+		options.addOption ("D", "list-dictionaries", false, "List all dictionaries");
+
+		
+		Settings settings = new Settings ();
 		ModelPool pool = null;
 		Model model = null;
-		try {
-			pool = new ModelPool (":memory:");
-			model = pool.getModel ();
-			
-			Settings settings = new Settings ();
-			Project project = model.addProject (new Date (), "", "", "");
-			Component component1 = model.addComponent(project, "Component 1");
-	
-			Status status1 = model.addStatus(project, "OPEN");
-			model.addStatus(project, "CLOSED");
-			model.setDefaultStatus (status1);
-	
-			
-			User user1 = model.addUser (project, "user 1");
-			Identity identity1a = model.addIdentity ("user1@mail.endl", "user1a", user1);
-			Identity identity1b = model.addIdentity ("user1@mail.endl", "user1b", user1);
-			
-			User user2 = model.addUser (project, "user 2");
-			Identity identity2a = model.addIdentity ("user2@mail.endl", "user2a", user2);
-			model.addIdentity ("user2@mail.endl", "user2b", user2);
-	
-			User user3 = model.addUser (project, "user 3");
-			Identity identity3a = model.addIdentity ("user3@mail.endl", "user3a", user3);
-			model.addIdentity ("user3@mail.endl", "user3b", user3);
-	
-			/*
-			Category cat1 = model.addCategory(project, "Corrective");
-			Category cat2 = model.addCategory(project, "Adaptive");
-			Category cat3 = model.addCategory(project, "Perfective");
-			*/
-	
-			Severity sev1 = model.addSeverity(project, "blocker");
-			model.addSeverity(project, "critical");
-			model.addSeverity(project, "major");
-			model.addSeverity(project, "normal");
-			model.addSeverity(project, "minor");
-			model.addSeverity(project, "trivial");
-			model.addSeverity(project, "enhancment");
-	
-			Priority priority1 = model.addPriority (project, "Immediate");
-			model.addPriority (project, "Urgent");
-			model.addPriority (project, "High");
-			model.addPriority (project, "Normal");
-			model.addPriority (project, "Low");
-	
-			
-			model.addCommit(project, identity1a, identity1a, new Date (), "commit 1", 1, 5, 5);
-			model.addCommit(project, identity1a, identity1a, new Date (), "commit 3", 1, 5, 5);
-			model.addCommit(project, identity1a, identity1a, new Date (), "commit 3", 1, 5, 5);
-	
-			model.addCommit(project, identity2a, identity2a, new Date (), "commit 4", 1, 5, 5);
-			model.addCommit(project, identity2a, identity2a, new Date (), "commit 5", 1, 5, 5);
-			model.addCommit(project, identity2a, identity2a, new Date (), "commit 6", 1, 5, 5);
-	
-			model.addCommit(project, identity3a, identity1a, new Date (), "commit 7", 1, 5, 5);
-			model.addCommit(project, identity3a, identity1a, new Date (), "commit 8", 1, 5, 5);
-			model.addCommit(project, identity3a, identity1a, new Date (), "commit 9", 1, 5, 5);
-	
-			
-	
-			model.addBug("1", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("2", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("3", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("4", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("5", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("6", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("7", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("8", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("9", identity1b, component1, "bug 1", new Date (), priority1, sev1);
-			model.addBug("10", identity1b, component1, "bug 1", new Date (), priority1, sev1);
 
+		CommandLineParser parser = new PosixParser ();
 		
-			// Config:
-			Configuration config = new Configuration ();
-			Parser parser = new Parser ();
-			String content = "\n"
-					+ " Reporter = {\n"
-					+ "  Name = \"Dummy Report\";\n"
-					+ "  Query = \"SELECT * From Bugs\";\n"
-					+ " };\n"
-					+ "\n";
-			
-			parser.parse (config, new File ("/foo/bar/baz"), content.getBytes());
+		try {
+			CommandLine cmd = parser.parse (options, args);
 
-			Reporter exporter = new Reporter (model);
-			for (ExporterConfig exconf : config.getExporterConfigs ()) {
+			if (cmd.hasOption ("help")) {
+				HelpFormatter formatter = new HelpFormatter ();
+				formatter.printHelp ("postprocessor", options);
+				return ;
+			}
+
+			if (cmd.hasOption ("db") == false) {
+				System.err.println ("Option --db is required");
+				return ;
+			}
+
+			if (cmd.hasOption ("config") == false) {
+				System.err.println ("Option --config is required");
+				return ;
+			}
+
+			Configuration config = new Configuration ();
+			Parser configParser = new Parser ();
+			try {
+				configParser.parse (config, new File (cmd.getOptionValue ("config")));
+			} catch (IOException e) {
+				System.err.println ("Could not read configuration file: " + e.getMessage ());
+				return ;
+			} catch (ParserException e) {
+				System.err.println ("Could not parse configuration file: " + e.getMessage ());
+				return ;
+			}
+
+			if (cmd.hasOption ("list-reports")) {
+				int i = 1;
+
+				for (ExporterConfig exconf : config.getExporterConfigs ()) {
+					System.out.println ("  (" + i + ") " + exconf.getName ());
+					i++;
+				}
+
+				return ;
+			}
+
+			
+			File dbf = new File (cmd.getOptionValue ("db"));
+			if (dbf.exists() == false || dbf.isFile () == false) {
+				System.err.println ("Invalid database file path");
+				return ;
+			}
+			
+			pool = new ModelPool (cmd.getOptionValue ("db"), 2);
+			model = pool.getModel ();
+
+			if (cmd.hasOption ("list-formats")) {
+				Reporter exporter = new Reporter (model);
+				int i = 1;
+
 				for (ReportWriter formatter : exporter.getWriters ()) {
-					String fileName = "test-export-" + exconf.getName () + formatter.getLabel () + "-" + formatter.getTypeName ();
-					System.out.println ("Export: " + fileName);
-					exporter.export (exconf, project, settings, formatter, fileName);
+					System.out.println ("  (" + i + ") " + formatter.getLabel ());
+					i++;
+				}
+
+				return ;
+			}
+			
+			if (cmd.hasOption ("list-projects")) {
+				for (Project proj : model.getProjects ()) {
+					System.out.println ("  " + proj.getId () + ": " + proj.getDate ());
+				}
+
+				return ;
+			}			
+			
+			Integer projId = null;
+			if (cmd.hasOption ("project") == false) {
+				System.err.println ("Option --project is required");
+				return ;
+			} else {
+				try {
+					projId = Integer.parseInt (cmd.getOptionValue ("project"));
+				} catch (NumberFormatException e) {
+					System.err.println ("Invalid project ID");
+					return ;
 				}
 			}
+
+			if (cmd.hasOption ("output") == false) {
+				System.err.println ("Option --output is required");
+				return ;
+			}
+
+			String outputPath = cmd.getOptionValue ("output");
+			model = pool.getModel ();
+			Project project = model.getProject (projId);
+
+			if (project == null) {
+				System.err.println ("Invalid project ID");
+				return ;
+			}
+
+			if (cmd.hasOption ("list-dictionaries")) {
+				List<at.ac.tuwien.inso.subcat.model.Dictionary> dictionaries = model.getDictionaries (project);
+				for (at.ac.tuwien.inso.subcat.model.Dictionary dict : dictionaries) {
+					System.out.println ("  (" + dict.getId () + ") " + dict.getName ());
+				}
+				return ;
+			}
+			
+			int bugDictId = -1;
+			if (cmd.hasOption ("bug-dictionary")) {
+				try {
+					bugDictId = Integer.parseInt(cmd.getOptionValue ("bug-dictionary"));
+					List<at.ac.tuwien.inso.subcat.model.Dictionary> dictionaries = model.getDictionaries (project);
+					boolean valid = false;
+
+					for (at.ac.tuwien.inso.subcat.model.Dictionary dict : dictionaries) {
+						if (dict.getId () == bugDictId) {
+							valid = true;
+							break;
+						}
+					}
+
+					if (valid == false) {
+						System.err.println ("Invalid bug dictionary ID");
+					}
+				} catch (NumberFormatException e) {
+					System.err.println ("Invalid bug dictionary ID");
+					return ;
+				}
+			}
+
+			int commitDictId = -1;
+			if (cmd.hasOption ("commit-dictionary")) {
+				try {
+					commitDictId = Integer.parseInt(cmd.getOptionValue ("commit-dictionary"));
+					List<at.ac.tuwien.inso.subcat.model.Dictionary> dictionaries = model.getDictionaries (project);
+					boolean valid = false;
+
+					for (at.ac.tuwien.inso.subcat.model.Dictionary dict : dictionaries) {
+						if (dict.getId () == commitDictId) {
+							valid = true;
+							break;
+						}
+					}
+					
+					if (valid == false) {
+						System.err.println ("Invalid commit dictionary ID");
+					}
+				} catch (NumberFormatException e) {
+					System.err.println ("Invalid commit dictionary ID");
+					return ;
+				}
+			}
+
+			if (cmd.hasOption ("format") == false) {
+				System.err.println ("Option --format is required");
+				return ;
+			}
+
+			Reporter exporter = new Reporter (model);
+			ReportWriter writer = null;
+			try {
+				int id = Integer.parseInt (cmd.getOptionValue ("format"));
+				if (id < 1 || id > exporter.getWriters ().size ()) {
+					System.err.println ("Invalid output format");
+					return ;
+				}
+				
+				writer = exporter.getWriters ().get (id - 1);
+			} catch (NumberFormatException e) {
+				System.err.println ("Invalid output format");
+				return ;
+			}
+
+			ExporterConfig exporterConfig = null;
+			if (cmd.hasOption ("report") == false) {
+				System.err.println ("Option --report is required");
+				return ;
+			} else {
+				try {
+					int id = Integer.parseInt (cmd.getOptionValue ("report"));
+					if (id < 1 || id > config.getExporterConfigs ().size ()) {
+						System.err.println ("Invalid reporter ID");
+						return ;
+					}
+					
+					exporterConfig = config.getExporterConfigs ().get (id -1);
+				} catch (NumberFormatException e) {
+					System.err.println ("Invalid reporter ID");
+					return ;
+				}
+			}
+
+			exporter.export (exporterConfig, project, commitDictId, bugDictId, settings, writer, outputPath);
+		} catch (ParseException e) {
+			System.err.println ("Parsing failed: " + e.getMessage ());
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			System.err.println ("Failed to create a database connection: " + e.getMessage ());
 		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParserException e) {
-			e.printStackTrace();
+			System.err.println ("Failed to create a database connection: " + e.getMessage ());
 		} catch (ReporterException e) {
-			e.printStackTrace();
-		}
-		
-		if (model != null) {
-			model.close ();
+			System.err.println ("Reporter Error: " + e.getMessage ());
+		} finally {
+			if (model != null) {
+				model.close ();
+			}
+			if (pool != null) {
+				pool.close ();
+			}
 		}
 	}
 }
